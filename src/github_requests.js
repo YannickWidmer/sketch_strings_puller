7c0535607802268ced17986a3c9b2528af723736
YannickWidmer <yannick.widmer@math.uzh.ch>
2019-01-30 14:47:51 -0800

    Add Log to github requests

--- src/github_requests.js
+++ src/github_requests.js
@@ -4,6 +4,7 @@ const Settings = require('sketch/settings')
 export function get_file(owner, repo, path, callback, error_callback){
     // creating token https://help.github.com/articles/creating-a-personal-access-token-for-the-command-line/
     // https://api.github.com/repos/${owner}/${repo}/git/blobs/${path}
+    console.log(`fetching https://api.github.com/repos/${owner}/${repo}/contents/${path}`)
     fetch(`https://api.github.com/repos/${owner}/${repo}/contents/${path}`,{
         method: "GET",
         headers: {
@@ -12,7 +13,7 @@ export function get_file(owner, repo, path, callback, error_callback){
           'Content-Type': 'application/json'
         }
     })
-        .then(response => response.json())
-        .then(text => callback(JSON.parse(Base64.decode(text.content))))
-        .catch(e => error_callback(`There was a problem with ${owner}/${repo}/${path}`))
+        .then(response => {console.log(response); return response.json();})
+        .then(text => {console.log('result: ' + text); callback(JSON.parse(Base64.decode(text.content)))})
+        .catch(e => error_callback(`There was a problem with ${owner}/${repo}/${path}.\nDo you have access to it? Are your credentials correct?`))
 }
